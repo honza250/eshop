6e14f4fd94a3822b0baaa033579c6a20c13f81f9
honza250 <honzator500@seznam.cz>
2025-05-08 14:22:10 +0200

    ejsProductsDB

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,24 @@
 import express from 'express';
 import path from 'path';
+import { db } from './db';
+import { products } from './db/schema';
 
 const app = express();
 app.use(express.json());
 
-// Úvodní stránka
+// Nastavíme EJS jako šablonovací engine
+app.set('views', path.join(__dirname, '../views'));
+app.set('view engine', 'ejs');
+
+// Úvodní stránka – pořád jako statické HTML
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../views/index.html'));
 });
 
-// Statická stránka s produkty
-app.get('/products', (req, res) => {
-  res.sendFile(path.join(__dirname, '../views/products.html'));
+// Dynamická stránka s produkty – přes EJS
+app.get('/products', async (req, res) => {
+  const result = await db.select().from(products);
+  res.render('products', { products: result });
 });
 
 export default app;
