f85d8a70d94929a651fd17502acb1f54fedbd743
honza250 <honzator500@seznam.cz>
2025-05-25 21:49:32 +0200

    seed

--- src/scripts/deleteProducts.ts
+++
@@ -1,9 +0,0 @@
-import { db } from '../db';
-import { products } from '../db/schema';
-
-async function dropProducts() {
-  await db.run(`DROP TABLE IF EXISTS products`);
-  console.log('Tabulka "products" byla smazána.');
-}
-
-dropProducts().catch(console.error);
